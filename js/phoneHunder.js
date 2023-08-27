674301fde186f2fe131080534a2aba30ac738de0
Fariha1865 <farihaamin187@gmail.com>
2023-08-28 03:02:46 +0600

    modal implemented

--- js/phoneHunder.js
+++ js/phoneHunder.js
@@ -49,7 +49,7 @@ function showDataInCard(data){
           <p class="text-center"> A stylish and powerful smartphone, featuring a brilliant display and advanced camera capabilities</p>
           <p class="text-lg font-bold">$999</p>
           <div class="card-actions mt-5">
-            <button class="btn btn-primary">Show Details</button>
+            <button onclick = "showDetails('${card.slug}')" class="btn btn-primary">Show Details</button>
           </div>
         </div>
         
@@ -96,3 +96,33 @@ showAllButton.addEventListener('click',function(){
     loadPhones(searchInput);
      
 })
+
+const modal_contents = document.getElementById('modal_contents');
+const showDetails = async(id) =>{
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    let data = await res.json();
+    data = data.data;
+    console.log(data)
+    my_modal.showModal();
+    // const modalElements = document.createElement('div');
+ 
+    modal_contents.style.padding= '40px';
+    modal_contents.innerHTML=`
+      <img src="${data.image}" alt="" class="w-[50%] h-[2%] mx-auto mb-5">
+      <p class="font-bold text-3xl mb-5">${data.name}</p>
+      <p class="mb-5"> A stylish and powerful smartphone, featuring a brilliant display and advanced camera capabilities</p>
+      <p class="mb-5"><span class="font-bold text-xl">Storage:</span> ${data.mainFeatures.storage}</p>
+      <p class="mb-5"><span class="font-bold text-xl">Display Size:</span> ${data.mainFeatures.displaySize.slice(0,19)}</p>
+      <p class="mb-5"><span class="font-bold text-xl">Chipset:</span> ${data.mainFeatures.chipSet.slice(0,17)}</p>
+      <p class="mb-5"><span class="font-bold text-xl">Memory:</span> ${data.mainFeatures.memory}</p>
+      <p class="mb-5"><span class="font-bold text-xl">Id:</span> ${data.slug}</p>
+      <p class="mb-5"><span class="font-bold text-xl">Release data:</span> ${data.releaseDate}</p>
+      <p class="mb-5"><span class="font-bold text-xl">Brand:</span> ${data.brand}</p>
+      <p class="mb-5"><span class="font-bold text-xl">GPS:</span> ${data?.others?.GPS? data.others.GPS: 'Unavailable'}</p>
+    `
+    // modal_contents.appendChild(modalElements)
+
+}
+
+loadPhones("13")
